103fee9bf6e922c870284c0098926773fbd5d4ea
StefOnMc <116894578+StefOnMc@users.noreply.github.com>
2023-06-24 22:23:12 +0200

    Update Npc.go

--- Npc.go
+++ Npc.go
@@ -8,18 +8,13 @@ import (
 
 // ExampleCommand contains our command parameters.
 type Npc struct {
-	// Each parameter requires the field to be exported in order for the command to work properly.
-	// The `cmd:""` struct tag may be specified to change the name of the parameter and its suffix.
-	// Supported type: int*, uint*, float*, string, bool, mgl64.Vec3, []cmd.Target, cmd.Enum ...
-	Number int `cmd:"number"`
-	// Wrapping the parameter type in `cmd.Optional` makes the parameter optional, meaning a cmd.Source
-	// does not need to supply it.
-	OptionalMessage cmd.Optional[string]
+	Message cmd.Varargs `cmd:"message"`
 }
 
 // Run will be called when the player runs the command. In this case, we will print the number back to the player
 func (c Npc) Run(source cmd.Source, output *cmd.Output) {
-	msg, ok := c.OptionalMessage.Load()
+	msg := strings.TrimSpace(string(a.Message))
+	
 	if len(msg) == 0 {
 
 		output.error("veuillier faire /npc <name> <taille> <x> <y> <z> ")
