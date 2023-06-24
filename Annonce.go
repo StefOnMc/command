1c985a5d0d8e18947dba0a2df6adedb9c1b9b438
StefOnMc <116894578+StefOnMc@users.noreply.github.com>
2023-06-24 11:01:54 +0200

    Update Annonce.go

--- Annonce.go
+++ Annonce.go
@@ -1,7 +1,26 @@
 package command
 
-import "github.com/df-mc/dragonfly/server/cmd"
+import (
+	"strings"
+	"github.com/df-mc/dragonfly/server/cmd"
+	"github.com/df-mc/dragonfly/server/player/chat"
+	"github.com/sandertv/gophertunnel/minecraft/text"
+)
 
 type Annonce struct {
 	Message cmd.Varargs `cmd:"message"`
+}
+
+func (a Annonce) Run(s cmd.Source, o *cmd.Output) {
+	msg := strings.TrimSpace(string(a.Message))
+	if len(msg) == 0 {
+		o.Error("erreur merci bien de mettre un message")
+		return
+	}
+	_, _ = chat.Global.WriteString(text.Colourf(strings.ReplaceAll(msg, "\\n", "\n")))
+}
+
+// Allow ...
+func (Annonce) Allow(s cmd.Source) bool {
+	return allow(s, true)
 }
