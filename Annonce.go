c714712163269189938bb6c5b0da756965e17b3e
StefOnMc <116894578+StefOnMc@users.noreply.github.com>
2023-06-24 12:52:57 +0200

    Update Annonce.go

--- Annonce.go
+++ Annonce.go
@@ -5,6 +5,7 @@ import (
 	"github.com/df-mc/dragonfly/server/cmd"
 	"github.com/df-mc/dragonfly/server/player/chat"
 	"github.com/sandertv/gophertunnel/minecraft/text"
+	"github.com/StefOnMc/proxy-mc"
 )
 
 type Annonce struct {
@@ -17,7 +18,7 @@ func (a Annonce) Run(s cmd.Source, o *cmd.Output) {
 		o.Error("erreur merci bien de mettre un message")
 		return
 	}
-	_, _ = chat.Global.WriteString(text.Colourf(strings.ReplaceAll(msg, "\\n", "\n")))
+	_, _ = chat.Global.WriteString(PREFIX, text.Colourf(strings.ReplaceAll(msg, "\\n", "\n")))
 }
 
 
